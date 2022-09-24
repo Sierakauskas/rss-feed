332dc28e1d0bfc2b3179f595ed5713d49443f156
mindaugas.arsulis <mindaugas.arsulis@insoft.lt>
2022-09-24 10:34:12 +0300

    refactor controller

--- rss-feed-ui/src/main/java/lt/rss/feed/controllers/IndexPageController.java
+++ rss-feed-ui/src/main/java/lt/rss/feed/controllers/IndexPageController.java
@@ -3,12 +3,10 @@ package lt.rss.feed.controllers;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
 import lt.rss.feed.bl.service.FeedService;
@@ -22,30 +20,28 @@ public class IndexPageController {
 
     @RequestMapping("/")
     public String mainPage(Model md) {
-        md.addAttribute("service", service);
-        md.addAttribute("feed", new Feed());
-        return "mainPage";
+        return mainPageAttributes(md, new Feed());
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String saveForm(Model model,
-            @ModelAttribute("feeds") Feed feed,
-            BindingResult bindingResult,
-            RedirectAttributes redirectAttributes) {
+    public String saveForm(Model md,
+            @ModelAttribute("feeds") Feed feed) {
         if (!StringUtils.isNoneBlank(feed.getUrl(), feed.getFeedName())) {
-            model.addAttribute("service", service);
-            model.addAttribute("feed", feed);
-            return "mainPage";
+            return mainPageAttributes(md, feed);
         }
         service.createOrUpdate(feed);
-        model.addAttribute("service", service);
-        model.addAttribute("feed", new Feed());
-        return "mainPage";
+        return mainPageAttributes(md, new Feed());
     }
 
     @RequestMapping("/view")
     public String editImage(Model md, @RequestParam(name = "id") Long id) {
         md.addAttribute("feed", service.getFeedViewItem(id));
         return "viewPage";
+    }
+
+    private String mainPageAttributes(Model md, Feed feed) {
+        md.addAttribute("service", service);
+        md.addAttribute("feed", feed);
+        return "mainPage";
     }
 }
